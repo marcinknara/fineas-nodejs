bd859d1093bcbcccd8753187a312b27587f08d3a
Marcin Knara <marcinknara@gmail.com>
2024-06-16 20:38:25 -0400

    Fix typo

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const configuration = new Configuration({
   basePath: PlaidEnvironments.sandbox,
   baseOptions: {
     headers: {
-      'PLAID-CLIENT-ID': procecss.env.PLAID_CLIENT_ID,
+      'PLAID-CLIENT-ID': process.env.PLAID_CLIENT_ID,
       'PLAID-SECRET': process.env.PLAID_SECRET,
     },
   },
